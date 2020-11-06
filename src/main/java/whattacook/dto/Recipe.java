5132fc2fec973782bd6780a0b472ce8dcda7d8a7
MartinG <martinjavierguilhem88@gmail.com>
2020-11-06 14:34:36 +0100

    B-3 Recipes crud

--- src/main/java/whattacook/dto/Recipe.java
+++ src/main/java/whattacook/dto/Recipe.java
@@ -1,4 +1,62 @@
 package whattacook.dto;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="recipe")
 public class Recipe {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="title")
+	private String title;
+	
+	@Column(name="text")
+	private String text;
+
+	public Recipe() {}
+
+	public Recipe(long id, String title, String text) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.text = text;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public String toString() {
+		return "Recipe [id=" + id + ", title=" + title + ", text=" + text + "]";
+	}
+		
 }
